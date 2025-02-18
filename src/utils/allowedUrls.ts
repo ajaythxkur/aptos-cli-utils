e7828db81f4459327d65fb62a825526276d624a9
Ajay Thakur <62602303+ajaythxkur@users.noreply.github.com>
2025-02-18 10:31:22 +0530

    Update allowedUrls.ts

--- src/utils/allowedUrls.ts
+++ src/utils/allowedUrls.ts
@@ -1,6 +1,6 @@
 export const allowedUrls = [
     "http://localhost:3000", 
-    "http://pumpit.pro",
+    "https://pumpit.pro",
     "https://testnet.pumpit.pro",
     "https://dev-pump.vercel.app"
 ]
